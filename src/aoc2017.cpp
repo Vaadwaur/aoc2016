751e0881c35330b781e43a230b77180b0b0ebaba
Jonas Hansson <jonas.ng.hansson@gmail.com>
2016-12-07 01:39:47 +0100

    Solution for day six.

--- src/aoc2017.cpp
+++ src/aoc2017.cpp
@@ -25,6 +25,7 @@ extern template void solve<kDay02>(bool, std::istream&, std::ostream&);
 extern template void solve<kDay03>(bool, std::istream&, std::ostream&);
 extern template void solve<kDay04>(bool, std::istream&, std::ostream&);
 extern template void solve<kDay05>(bool, std::istream&, std::ostream&);
+extern template void solve<kDay06>(bool, std::istream&, std::ostream&);
 
 uint64_t
 run(eDay day, bool part1, std::ostream& os)
@@ -51,6 +52,9 @@ run(eDay day, bool part1, std::ostream& os)
 		break;
 	case kDay05:
 		solve<kDay05>(part1, is, os);
+		break;
+	case kDay06:
+		solve<kDay06>(part1, is, os);
 		break;
 	default:
 		return 0;
---
+++ src/day06/day06.cpp
@@ -0,0 +1,73 @@
+#include <algorithm>
+#include <fstream>
+#include <functional>
+#include <utility>
+#include <days.h>
+
+namespace aoc2017 {
+
+using tData = std::array<std::array<char, 26>, 8>;
+using tDataDataIt = tData::value_type::const_iterator;
+
+static tData
+get_data(std::istream& _is)
+{
+	tData data{};
+	std::string line;
+	while (std::getline(_is, line)) {
+		for (int i{}; i < 8; ++i) {
+			++data[i][line[i] - 'a'];
+		}
+	}
+
+	return data;
+}
+
+static std::string
+get_code(tData const& data, std::function<tDataDataIt(tDataDataIt,tDataDataIt)> const& fun)
+{
+	std::string code;
+	for (size_t i{}; i < data.size(); ++i) {
+		auto it = fun(std::cbegin(data[i]), std::cend(data[i]));
+		char diff = static_cast<char>(std::distance(std::cbegin(data[i]), it));
+		code += 'a' + diff;
+	}
+
+	return code;
+}
+
+static auto
+part1(std::istream& _is)
+{
+	auto data = get_data(_is);
+	auto code = get_code(data, [](tDataDataIt first, tDataDataIt last) {
+		return std::max_element(first, last);
+	});
+
+	return code;
+}
+
+static auto
+part2(std::istream& _is)
+{
+	auto data = get_data(_is);
+	auto code = get_code(data, [](tDataDataIt first, tDataDataIt last) {
+		return std::min_element(first, last);
+	});
+
+	return code;
+}
+
+template<> void
+solve<kDay06>(bool _part1, std::istream& _is, std::ostream& _os)
+{
+	_os << "day06:";
+	if (_part1) {
+		_os << "part1:" << part1(_is);
+	}
+	else {
+		_os << "part2:" << part2(_is);
+	}
+}
+
+} // namespace aoc2017
